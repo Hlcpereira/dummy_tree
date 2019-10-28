183e6d888107af25c5a6158ae3938a7b68dc2af7
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-10-28 17:01:07 -0300

    [FOD Devices][REQUIRED/REFERENCE] Adapt to our modified FOD impl
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -1,5 +1,2 @@
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.<something>
-
-PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
